c71e2d4c51a3198517af6736eba02b2e9d6da1b5
Dropsleutel69 <timvdheide2008@gmail.com>
2026-04-24 16:22:09 +0200

    .
    Built player class and player name intro

---
+++ Alchemist Trial/Alchemist Trial/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alchemist_Trial
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+
+        public Player (string name, int health, int damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+
+            if (Health < 0)
+            {
+                Health = 0;
+            }
+        }
+
+        public void Heal(int amount)
+        {
+            Health += amount;
+        }
+
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine("------ Player Stats ------");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Damage: {Damage}");
+            Console.WriteLine("--------------------------");
+        }
+
+
+    }
+}
---
+++ Alchemist Trial/Alchemist Trial/Program.cs
@@ -0,0 +1,10 @@
+﻿using Alchemist_Trial;
+
+Console.WriteLine("The Academy calls upon a new alchemist");
+Console.Write("What is your name, apprentice?");
+
+string playerName = Console.ReadLine();
+
+Player player = new Player(playerName, 100, 15);
+
+player.ShowStats();
